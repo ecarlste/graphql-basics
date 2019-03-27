cf3d7b5883b33f77065243f8096ab3d899e4266d
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-27 14:26:47 -0600

    Extracted graphql schema into separate file

--- src/index.js
+++ src/index.js
@@ -71,69 +71,6 @@ let comments = [
   }
 ];
 
-const typeDefs = `
-  type Query {
-    users(query: String): [User!]!
-    me: User!
-    posts(query: String): [Post!]!
-    post: Post!
-    comments: [Comment!]!
-  }
-
-  type Mutation {
-    createUser(input: CreateUserInput!): User!
-    deleteUser(id: ID!): User!
-    createPost(input: CreatePostInput!): Post!
-    deletePost(id: ID!): Post!
-    createComment(input: CreateCommentInput!): Comment!
-    deleteComment(id: ID!): Comment!
-  }
-
-  input CreateUserInput {
-    name: String!
-    email: String!
-    age: Int
-  }
-
-  type User {
-    id: ID!
-    name: String!
-    email: String!
-    age: Int
-    posts: [Post!]!
-    comments: [Comment!]!
-  }
-
-  input CreatePostInput {
-    title: String!
-    body: String!
-    published: Boolean!
-    author: ID!
-  }
-
-  type Post {
-    id: ID!
-    title: String!
-    body: String!
-    published: Boolean!
-    author: User!
-    comments: [Comment!]!
-  }
-
-  input CreateCommentInput {
-    text: String!
-    author: ID!
-    post: ID!
-  }
-
-  type Comment {
-    id: ID!
-    text: String!
-    author: User!
-    post: Post!
-  }
-`;
-
 const resolvers = {
   Query: {
     users(_, args) {
@@ -299,7 +236,7 @@ const resolvers = {
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers
 });
 
