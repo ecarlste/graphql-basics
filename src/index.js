3bc1e2f4d1180ce59b42c75cddfee02042abdfbb
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-27 03:03:19 -0600

    Setup initial prototype of users query

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 import { GraphQLServer } from 'graphql-yoga';
 
+const users = [
+  {
+    id: '1',
+    name: 'Erik',
+    email: 'erik@test.com',
+    age: 39
+  },
+  {
+    id: '2',
+    name: 'Sarah',
+    email: 'sarah@test.com'
+  },
+  {
+    id: '3',
+    name: 'Mike',
+    email: 'mike@test.com'
+  }
+];
+
 const typeDefs = `
   type Query {
+    users(query: String): [User!]!
     me: User!
     post: Post!
   }
@@ -23,6 +43,15 @@ const typeDefs = `
 
 const resolvers = {
   Query: {
+    users(_, args) {
+      if (!args.query) {
+        return users;
+      }
+
+      return users.filter(user => {
+        return user.name.toLowerCase().includes(args.query.toLowerCase());
+      });
+    },
     me() {
       return {
         id: '123',
