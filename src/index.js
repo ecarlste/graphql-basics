1e5bb04c68592aa2f565981dad29d43351bb9a73
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-27 00:46:37 -0600

    Super simple GraphQL API/server w/ graphql-yoga

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-console.log('Hello GraphQL!');
+import { GraphQLServer } from 'graphql-yoga';
+
+const typeDefs = `
+  type Query {
+    hello: String!
+    name: String!
+    location: String!
+    bio: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello() {
+      return 'this is my first query';
+    },
+    name() {
+      return 'Erik';
+    },
+    location() {
+      return 'Boulder, CO';
+    },
+    bio() {
+      return 'Definitely one bad maammaa jamma!?';
+    }
+  }
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+});
+
+server.start(() => console.log('GraphQL server is up...'));
